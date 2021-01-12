37a26498b69efd6778c12e273ab31932348a6102
shun <869588058@qq.com>
2021-01-11 21:34:45 -0500

    django 2.2 -> 3.1

--- gosc/settings.py
+++ gosc/settings.py
@@ -10,11 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
-import os
+# import os
+#
+# # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -65,7 +67,7 @@ ROOT_URLCONF = 'gosc.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates'), ],
+        'DIRS': [Path('/').joinpath(BASE_DIR, 'templates'), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -129,10 +131,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'collect_static')
+STATIC_ROOT = Path('/').joinpath(BASE_DIR, 'collect_static')
 # 静态文件查找路径
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    Path('/').joinpath(BASE_DIR, 'static'),
 ]
 
 # session 有效期设置
