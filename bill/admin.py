cb106b331b047950d6f14e4c6b818bc4d82b861c
shun <869588058@qq.com>
2022-03-10 04:06:57 -0500

    define model, Bill/PaymentHistory/User(VO)PointAccount

--- bill/admin.py
+++ bill/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Bill, PaymentHistory, UserPointAccount, VoPointAccount
+
+
+@admin.register(Bill)
+class BillAdmin(admin.ModelAdmin):
+    list_display = ('id', 'type', 'status', 'amounts', 'creation_time', 'resource_type',
+                    'service_id', 'instance_id', 'order_id', 'owner_type', 'user_id', 'vo_id')
+    list_display_links = ('id',)
+    list_filter = ('type', 'resource_type', 'owner_type')
+    search_fields = ('id', 'order_id', 'instance_id', 'user_id', 'vo_id', 'service_id')
+
+
+@admin.register(PaymentHistory)
+class PaymentHistoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'type', 'amounts', 'before_payment', 'after_payment', 'payment_time',
+                    'payer_type', 'payer_id', 'payer_name')
+    list_display_links = ('id',)
+    list_filter = ('type', 'payer_type')
+    search_fields = ('id', 'payer_id', 'payer_name')
+    raw_id_fields = ('bill',)
+
+
+@admin.register(UserPointAccount)
+class UserPointAccountAdmin(admin.ModelAdmin):
+    list_display = ('id', 'balance', 'user',)
+    list_display_links = ('id',)
+    list_select_related = ('user',)
+
+
+@admin.register(VoPointAccount)
+class VoPointAccountAdmin(admin.ModelAdmin):
+    list_display = ('id', 'balance', 'vo',)
+    list_display_links = ('id',)
+    list_select_related = ('vo',)
--- bill/models.py
+++ bill/models.py
@@ -1,3 +1,108 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from utils.model import UuidModel, OwnerType
+from order.models import ResourceType
+from users.models import UserProfile
+from vo.models import VirtualOrganization
+
+
+class UserPointAccount(UuidModel):
+    balance = models.DecimalField(verbose_name=_('金额'), max_digits=10, decimal_places=2)
+    user = models.OneToOneField(to=UserProfile, on_delete=models.SET_NULL, null=True, default=None)
+    creation_time = models.DateTimeField(verbose_name=_('创建时间'), auto_now_add=True)
+
+    class Meta:
+        verbose_name = _('用户账户')
+        verbose_name_plural = verbose_name
+        db_table = 'user_point_account'
+        ordering = ['-creation_time']
+
+    def __repr__(self):
+        if self.user:
+            return f'UserPointAccount[{self.user.username}]<{self.balance}>'
+
+        return f'UserPointAccount<{self.balance}>'
+
+
+class VoPointAccount(UuidModel):
+    balance = models.DecimalField(verbose_name=_('金额'), max_digits=10, decimal_places=2)
+    vo = models.OneToOneField(to=VirtualOrganization, on_delete=models.SET_NULL, null=True, default=None)
+    creation_time = models.DateTimeField(verbose_name=_('创建时间'), auto_now_add=True)
+
+    class Meta:
+        verbose_name = _('VO组账户')
+        verbose_name_plural = verbose_name
+        db_table = 'vo_point_account'
+        ordering = ['-creation_time']
+
+    def __repr__(self):
+        if self.vo:
+            return f'VoPointAccount[{self.vo.name}]<{self.balance}>'
+
+        return f'VoPointAccount<{self.balance}>'
+
+
+class Bill(UuidModel):
+    class Type(models.TextChoices):
+        METER = 'meter', _('计费')
+        PREPAID = 'prepaid', _('预付费')
+        REFUND = 'refund', _('退款')
+
+    class Status(models.TextChoices):
+        UNPAID = 'unpaid', _('待支付')
+        PREPAID = 'prepaid', _('已支付')
+        CANCELLED = 'cancelled', _('作废')
+
+    type = models.CharField(verbose_name=_('账单类型'), max_length=16, choices=Type.choices)
+    status = models.CharField(verbose_name=_('账单状态'), max_length=16, choices=Status.choices)
+    amounts = models.DecimalField(verbose_name=_('金额'), max_digits=10, decimal_places=2)
+
+    order_id = models.CharField(verbose_name=_('订单ID'), max_length=36, blank=True, default='')
+    resource_type = models.CharField(
+        verbose_name=_('资源类型'), max_length=16, choices=ResourceType.choices)
+    service_id = models.CharField(verbose_name=_('服务ID'), max_length=36, blank=True, default='')
+    instance_id = models.CharField(verbose_name=_('资源实例ID'), max_length=36, default='')
+
+    creation_time = models.DateTimeField(verbose_name=_('创建时间'), auto_now_add=True)
+    user_id = models.CharField(verbose_name=_('用户ID'), max_length=36, blank=True, default='')
+    vo_id = models.CharField(verbose_name=_('VO组ID'), max_length=36, blank=True, default='')
+    owner_type = models.CharField(verbose_name=_('所有者类型'), max_length=8, choices=OwnerType.choices)
+
+    class Meta:
+        verbose_name = _('账单')
+        verbose_name_plural = verbose_name
+        db_table = 'bill'
+        ordering = ['-creation_time']
+
+    def __repr__(self):
+        return f'Bill[{self.id}]<{self.get_type_display()}, {self.get_status_display()}, {self.amounts}>'
+
+
+class PaymentHistory(UuidModel):
+    class Type(models.TextChoices):
+        RECHARGE = 'recharge', _('充值')
+        PAYMENT = 'payment', _('支付')
+        REFUND = 'refund', _('退款')
+
+    payer_id = models.CharField(verbose_name=_('付款人ID'), max_length=36, blank=True, default='',
+                                help_text='user id or vo id')
+    payer_name = models.CharField(verbose_name=_('付款人名称'), max_length=255, blank=True, default='',
+                                  help_text='username or vo name')
+    payer_type = models.CharField(verbose_name=_('付款人类型'), max_length=8, choices=OwnerType.choices)
+    amounts = models.DecimalField(verbose_name=_('金额'), max_digits=10, decimal_places=2)
+    before_payment = models.DecimalField(verbose_name=_('支付前余额'), max_digits=10, decimal_places=2)
+    after_payment = models.DecimalField(verbose_name=_('支付后余额'), max_digits=10, decimal_places=2)
+    payment_time = models.DateTimeField(verbose_name=_('支付时间'), auto_now_add=True)
+    type = models.CharField(verbose_name=_('支付类型'), max_length=16, choices=Type.choices)
+    bill = models.OneToOneField(to=Bill, on_delete=models.SET_NULL, null=True, blank=True, default=None)
+    remark = models.CharField(verbose_name=_('备注信息'), max_length=255, blank=True, default='')
+
+    class Meta:
+        verbose_name = _('支付记录')
+        verbose_name_plural = verbose_name
+        db_table = 'payment_history'
+        ordering = ['-payment_time']
+
+    def __repr__(self):
+        return f'PaymentHistory[{self.id}]<{self.get_type_display()}, {self.amounts}>'
