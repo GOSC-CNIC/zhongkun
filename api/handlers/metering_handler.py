7e283d8700254dccee831352945411cbbbdea169
Chen <735472584@qq.com>
2022-05-09 23:17:11 -0400

    AggregateMeteringByServer api and testcase

--- api/handlers/metering_handler.py
+++ api/handlers/metering_handler.py
@@ -26,17 +26,17 @@ class MeteringHandler:
         user_id = params['user_id']
 
         ms_mgr = MeteringServerManager()
-        if view.is_as_admin_request(request):
+        if view.is_as_admin_request(request):   
             queryset = ms_mgr.filter_server_metering_by_admin(
                 user=request.user, service_id=service_id, server_id=server_id, date_start=date_start,
                 date_end=date_end, vo_id=vo_id, user_id=user_id
             )
-        elif vo_id:
-            queryset = ms_mgr.filter_vo_server_metering(
+        elif vo_id:     
+            queryset = ms_mgr.filter_vo_server_metering(    
                 user=request.user, service_id=service_id, server_id=server_id, date_start=date_start,
                 date_end=date_end, vo_id=vo_id
             )
-        else:
+        else:           
             queryset = ms_mgr.filter_user_server_metering(
                 user=request.user, service_id=service_id, server_id=server_id, date_start=date_start,
                 date_end=date_end
@@ -105,4 +105,88 @@ class MeteringHandler:
             'service_id': service_id,
             'server_id': server_id,
             'user_id': user_id
+        }
+
+    def list_aggregation_by_server(self, view: CustomGenericViewSet, request):
+        """
+        列举云主机计量计费聚合信息
+        """
+        try:
+            params = self.list_aggregation_by_server_validate_params(view=view, request=request)
+        except errors.Error as exc:
+            return view.exception_response(exc)
+
+        date_start = params['date_start']
+        date_end = params['date_end']
+        user_id = params['user_id']
+        service_id = params['service_id']
+        vo_id = params['vo_id']
+
+        ms_mgr = MeteringServerManager()
+        if view.is_as_admin_request(request):       
+            queryset = ms_mgr.aggregate_server_metering_by_uuid_by_admin(
+                user=request.user, date_start=date_start, date_end=date_end, user_id=user_id,
+                service_id=service_id, vo_id=vo_id
+            )
+        elif vo_id:     
+            queryset = ms_mgr.aggregate_server_metering_by_uuid_by_vo(
+                user=request.user, service_id=service_id, date_start=date_start,
+                date_end=date_end, vo_id=vo_id
+            )
+        else:         
+            queryset = ms_mgr.aggregate_server_metering_by_uuid_by_user(
+                user=request.user, date_start=date_start, date_end=date_end, service_id=service_id
+            )
+
+        try:
+            data = view.paginate_queryset(queryset)
+            data = ms_mgr.aggregate_by_server_mixin_data(data)
+            return view.get_paginated_response(data)
+        except Exception as exc:
+            return view.exception_response(exc)
+
+    @staticmethod
+    def list_aggregation_by_server_validate_params(view: CustomGenericViewSet, request) -> dict:    
+        date_start = request.query_params.get('date_start', None)
+        date_end = request.query_params.get('date_end', None)
+        user_id = request.query_params.get('user_id', None)
+        service_id = request.query_params.get('service_id', None)
+        vo_id = request.query_params.get('vo_id', None)
+
+        now_date = timezone.now().date()
+
+        if vo_id is not None and not vo_id:
+            raise errors.InvalidArgument(message=_('参数“vo_id”的值无效'))
+
+        if date_start is not None:
+            try:
+                date_start = date.fromisoformat(date_start)
+            except (TypeError, ValueError):
+                raise errors.InvalidArgument(message=_('参数“date_start”的值无效的日期格式'))
+        else:                       # 默认当月起始时间
+            date_start = now_date.replace(day=1)
+
+        if date_end is not None:
+            try:
+                date_end = date.fromisoformat(date_end)
+            except (TypeError, ValueError):
+                raise errors.InvalidArgument(message=_('参数“date_end”的值无效的日期格式'))
+        else:
+            date_end = now_date     # 默认当月当前日期
+
+        if user_id is not None and not view.is_as_admin_request(request):
+            raise errors.BadRequest(message=_('参数“user_id”仅以管理员身份查询时允许使用'))
+
+        if service_id is not None and not service_id:
+            raise errors.InvalidArgument(message=_('参数“service_id”的值无效'))
+
+        if user_id is not None and vo_id is not None:
+            raise errors.BadRequest(message=_('参数“user_id”和“vo_id”不能同时提交'))
+
+        return {
+            'date_start': date_start,
+            'date_end':  date_end,
+            'user_id': user_id,
+            'service_id': service_id,
+            'vo_id': vo_id
         }
--- metering/managers.py
+++ metering/managers.py
@@ -1,11 +1,12 @@
 from datetime import date
 
 from django.utils.translation import gettext as _
-from django.db.models import Subquery
+from django.db.models import Subquery, Sum
 
 from core import errors
 from service.managers import ServiceManager
 from servers.managers import ServerManager
+from servers.models import Server, ServerArchive
 from vo.managers import VoManager
 from utils.model import OwnerType
 from .models import MeteringServer
@@ -50,7 +51,7 @@ class MeteringServerManager:
             date_end=date_end, vo_id=vo_id
         )
 
-    def filter_server_metering_by_admin(
+    def filter_server_metering_by_admin(    
             self, user,
             service_id: str = None,
             server_id: str = None,
@@ -64,24 +65,25 @@ class MeteringServerManager:
 
         :rasies: AccessDenied, NotFound, Error
         """
-        if user.is_federal_admin():
+        if user.is_federal_admin():     
             return self.filter_server_metering_queryset(
                 service_id=service_id, server_id=server_id, date_start=date_start, date_end=date_end,
                 vo_id=vo_id, user_id=user_id
             )
 
-        if server_id:
-            server_or_archieve = ServerManager.get_server_or_archive(server_id=server_id)
-            if server_or_archieve is None:
+        if server_id:                    
+            server_or_archieve = ServerManager.get_server_or_archive(server_id=server_id) 
+            
+            if server_or_archieve is None:         
                 return MeteringServer.objects.none()
-
-            if service_id:
+            
+            if service_id:      
                 if service_id != server_or_archieve.service_id:
                     return MeteringServer.objects.none()
-            else:
+            else:              
                 service_id = server_or_archieve.service_id
 
-        if service_id:
+        if service_id:      
             service = ServiceManager.get_service_if_admin(user=user, service_id=service_id)
             if service is None:
                 raise errors.AccessDenied(message=_('您没有指定服务的访问权限'))
@@ -98,7 +100,7 @@ class MeteringServerManager:
 
         return queryset
 
-    def filter_server_metering_queryset(
+    def filter_server_metering_queryset(       
             self, service_id: str = None,
             server_id: str = None,
             date_start: date = None,
@@ -133,6 +135,124 @@ class MeteringServerManager:
             lookups['owner_type'] = OwnerType.VO.value
             lookups['vo_id'] = vo_id
 
-        queryset = self.get_metering_server_queryset()
-        return queryset.filter(**lookups).order_by('-creation_time')
+        queryset = self.get_metering_server_queryset()      
+        return queryset.filter(**lookups).order_by('-creation_time')    
 
+    def aggregate_server_metering_by_uuid_by_admin(
+            self, user,
+            date_start: date = None,
+            date_end: date = None,
+            user_id: str = None,
+            service_id: str = None,
+            vo_id: str = None
+    ):
+        """
+            管理员获取以server_id聚合的查询集
+        """
+        if user.is_federal_admin():
+            queryset = self.filter_server_metering_queryset(
+                service_id=service_id, date_start=date_start, date_end=date_end, user_id=user_id, vo_id=vo_id
+            )
+            return self.aggregate_queryset_by_server(queryset)
+
+        if service_id:
+            service = ServiceManager.get_service_if_admin(user=user, service_id=service_id)
+            if service is None:
+                raise errors.AccessDenied(message=_('您没有指定服务的访问权限'))
+
+        queryset = self.filter_server_metering_queryset(
+            service_id=service_id, date_start=date_start, date_end=date_end, user_id=user_id, vo_id=vo_id
+        )
+
+        if not service_id:
+            qs = ServiceManager.get_all_has_perm_service(user)  
+            subq = Subquery(qs.values_list('id', flat=True))   
+            queryset = queryset.filter(service_id__in=subq)
+
+        return self.aggregate_queryset_by_server(queryset)
+
+    def aggregate_server_metering_by_uuid_by_user(
+            self, user,
+            date_start: date = None,
+            date_end: date = None,
+            service_id: str = None
+    ):
+        """
+        普通用户获取自己名下以server_id聚合的查询集
+        """
+        queryset = self.filter_server_metering_queryset(
+            service_id=service_id, date_start=date_start,
+            date_end=date_end, user_id=user.id
+        )
+        return self.aggregate_queryset_by_server(queryset)
+
+    def aggregate_server_metering_by_uuid_by_vo(
+            self, user,
+            date_start: date = None,
+            date_end: date = None,
+            service_id: str = None,
+            vo_id: str = None
+    ):
+        """
+        指定vo组下以server_id聚合的查询集
+        """
+        VoManager().get_has_read_perm_vo(vo_id=vo_id, user=user)
+        queryset = self.filter_server_metering_queryset(
+            service_id=service_id, date_start=date_start,
+            date_end=date_end, vo_id=vo_id
+        )
+        return self.aggregate_queryset_by_server(queryset)
+
+    @staticmethod
+    def aggregate_queryset_by_server(queryset):
+        """
+        聚合云主机计量数据
+        """
+        queryset = queryset.values('server_id').annotate(
+            total_cpu_hours=Sum('cpu_hours'),
+            total_ram_hours=Sum('ram_hours'),
+            total_disk_hours=Sum('disk_hours'),
+            total_public_ip_hours=Sum('public_ip_hours'),
+            total_original_amount=Sum('original_amount')
+        ).order_by('server_id')
+
+        return queryset
+
+    @staticmethod
+    def aggregate_by_server_mixin_data(data: list):
+        """
+        按server id聚合数据分页后混合其他数据
+        """
+        server_ids = [i['server_id'] for i in data]
+        servers = Server.objects.filter(id__in=server_ids).values('id', 'ipv4', 'ram', 'vcpus', 'service__name')
+        archives = ServerArchive.objects.filter(
+            server_id__in=server_ids, archive_type=ServerArchive.ArchiveType.ARCHIVE.value
+        ).values('server_id', 'ipv4', 'ram', 'vcpus', 'service__name')
+
+        server_dict = {}
+        for s in servers:
+            d = {
+                'service_name': s.pop('service__name', None),
+                'server': s
+            }
+            server_dict[s['id']] = d
+
+        for a in archives:
+            server_id = a['id'] = a.pop('server_id', None)
+            if server_id and server_id not in server_dict:
+                d = {
+                    'service_name': a.pop('service__name', None),
+                    'server': a
+                }
+                server_dict[server_id] = d
+
+        for i in data:
+            i: dict
+            sid = i['server_id']
+            if sid in server_dict:
+                i.update(server_dict[sid])
+            else:
+                i['service_name'] = None
+                i['server'] = None
+
+        return data
