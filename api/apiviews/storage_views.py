2a7adf5a4a934a1cf4bda72eb4c86683aa15672f
shun <869588058@qq.com>
2023-04-24 17:11:33 +0800

    add AdminStorageStatistics api and testcase

--- api/apiviews/storage_views.py
+++ api/apiviews/storage_views.py
@@ -1,6 +1,8 @@
 from django.utils.translation import gettext_lazy, gettext as _
+from django.db.models import Sum
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.serializers import Serializer
+from rest_framework.response import Response
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 
@@ -9,7 +11,9 @@ from api.viewsets import StorageGenericViewSet
 from api.paginations import DefaultPageNumberPagination
 from api.serializers import storage as storage_serializers
 from storage.managers import ObjectsServiceManager
-from storage.models import ObjectsService
+from storage.models import ObjectsService, Bucket, BucketArchive
+from utils.paginators import NoPaginatorInspector
+from utils.time import iso_utc_to_datetime
 
 
 class ObjectsServiceViewSet(StorageGenericViewSet):
@@ -100,4 +104,99 @@ class ObjectsServiceViewSet(StorageGenericViewSet):
         if self.action == 'list':
             return storage_serializers.ObjectsServiceSerializer
 
+        return Serializer
+
+
+class StorageStatisticsViewSet(StorageGenericViewSet):
+    permission_classes = [IsAuthenticated]
+    pagination_class = None
+    lookup_field = 'id'
+    # lookup_value_regex = '[^/]+'
+
+    @swagger_auto_schema(
+        operation_summary=gettext_lazy('查询对象存储统计信息'),
+        manual_parameters=[
+            openapi.Parameter(
+                name='time_start',
+                in_=openapi.IN_QUERY,
+                type=openapi.TYPE_STRING,
+                required=False,
+                description=f'时间段起（含），ISO8601格式：YYYY-MM-ddTHH:mm:ssZ'
+            ),
+            openapi.Parameter(
+                name='time_end',
+                in_=openapi.IN_QUERY,
+                type=openapi.TYPE_STRING,
+                required=False,
+                description=f'时间段止（不含），ISO8601格式：YYYY-MM-ddTHH:mm:ssZ'
+            ),
+        ],
+        paginator_inspectors=[NoPaginatorInspector],
+        responses={
+            200: ''
+        }
+    )
+    def list(self, request, *args, **kwargs):
+        """
+        查询对象存储统计信息
+
+            http code 200：
+            {
+              "current_bucket_count": 3,    # 当前存储桶总数量
+              "new_bucket_count": 2,        # 指定时间段内创建的正常使用的桶的数量，不指定时间段时统计全部桶
+              "new_bucket_delete_count": 1, # 指定时间段内创建的已删除的桶的数量（创建后删除的），不指定时间段时统计全部删除桶
+              "total_storage_size": 11607311,   # 指定时间段内创建的桶（不含删除的桶）的总存储容量，byte，不指定时间段时统计全部桶
+              "total_object_count": 3           # 指定时间段内创建的存储桶（不含删除的桶）总对象数量，不指定时间段时统计全部桶
+            }
+        """
+        time_start = request.query_params.get('time_start', None)
+        time_end = request.query_params.get('time_end', None)
+
+        try:
+            if time_start is not None:
+                time_start = iso_utc_to_datetime(time_start)
+                if time_start is None:
+                    raise errors.InvalidArgument(message=_('参数“time_start”的值无效的时间格式'))
+
+            if time_end is not None:
+                time_end = iso_utc_to_datetime(time_end)
+                if time_end is None:
+                    raise errors.InvalidArgument(message=_('参数“time_end”的值无效的时间格式'))
+
+            if time_start and time_end:
+                if time_start >= time_end:
+                    raise errors.InvalidArgument(message=_('参数“time_start”时间必须超前“time_end”时间'))
+        except Exception as exc:
+            return self.exception_response(exc)
+
+        lookups = {}
+        if time_start:
+            lookups['creation_time__gte'] = time_start
+
+        if time_end:
+            lookups['creation_time__lte'] = time_end
+
+        try:
+            bucket_count = Bucket.objects.count()
+            if lookups:
+                new_bucket_count = Bucket.objects.filter(**lookups).count()
+                new_bucket_delete_count = BucketArchive.objects.filter(**lookups).count()
+            else:
+                new_bucket_count = bucket_count
+                new_bucket_delete_count = BucketArchive.objects.count()
+
+            r = Bucket.objects.filter(**lookups).aggregate(
+                total_size=Sum('storage_size'), total_count=Sum('object_count'))
+
+            return Response(data={
+                'current_bucket_count': bucket_count,
+                'new_bucket_count': new_bucket_count,
+                'new_bucket_delete_count': new_bucket_delete_count,
+                'total_storage_size': r['total_size'] if r['total_size'] else 0,
+                'total_object_count': r['total_count'] if r['total_count'] else 0
+            })
+        except Exception as exc:
+            return self.exception_response(exc)
+
+    def get_serializer_class(self):
         return Serializer
--- api/tests/test_storage.py
+++ api/tests/test_storage.py
@@ -1,7 +1,10 @@
 from urllib import parse
+from datetime import timedelta
 
 from django.urls import reverse
+from django.utils import timezone
 
+from storage.models import Bucket
 from utils.test import get_or_create_user, get_or_create_storage_service
 from . import MyAPITestCase
 
@@ -60,3 +63,165 @@ class ObjectsServiceTests(MyAPITestCase):
         self.assertEqual(r.status_code, 200)
         self.assertEqual(r.data['count'], 1)
         self.assertEqual(len(r.data['results']), 1)
+
+
+class StorageStatisticsTests(MyAPITestCase):
+    def setUp(self):
+        self.service = get_or_create_storage_service()
+        self.user = get_or_create_user(username='lilei@xx.com')
+
+    def test_storage_statistics(self):
+        url = reverse('api:admin-storage-statistics-list')
+        r = self.client.get(url)
+        self.assertErrorResponse(status_code=401, code='NotAuthenticated', response=r)
+
+        self.client.force_login(self.user)
+        r = self.client.get(url)
+        self.assertEqual(r.status_code, 200)
+        self.assertKeysIn(keys=[
+            'current_bucket_count', 'new_bucket_count', 'new_bucket_delete_count',
+            'total_storage_size', 'total_object_count'
+        ], container=r.data)
+        self.assertEqual(r.data['current_bucket_count'], 0)
+        self.assertEqual(r.data['new_bucket_count'], 0)
+        self.assertEqual(r.data['new_bucket_delete_count'], 0)
+        self.assertEqual(r.data['total_storage_size'], 0)
+        self.assertEqual(r.data['total_object_count'], 0)
+
+        nt = timezone.now()
+        # query "time_end" < "time_start"
+        query = parse.urlencode(query={
+            'time_start': '2023-04-24T08:08:25Z', 'time_end': '2023-04-24T08:08:24Z'
+        })
+        r = self.client.get(f'{url}?{query}')
+        self.assertErrorResponse(status_code=400, code='InvalidArgument', response=r)
+
+        # invalid "time_start"
+        query = parse.urlencode(query={
+            'time_start': '2023-04-24T08:082Z'
+        })
+        r = self.client.get(f'{url}?{query}')
+        self.assertErrorResponse(status_code=400, code='InvalidArgument', response=r)
+
+        query = parse.urlencode(query={
+            'time_start': '2023-03-24T00:00:00Z', 'time_end': '2023-04-24T00:00:00Z'
+        })
+        r = self.client.get(f'{url}?{query}')
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.data['current_bucket_count'], 0)
+        self.assertEqual(r.data['new_bucket_count'], 0)
+        self.assertEqual(r.data['new_bucket_delete_count'], 0)
+        self.assertEqual(r.data['total_storage_size'], 0)
+        self.assertEqual(r.data['total_object_count'], 0)
+
+        # init data
+        bucket1 = Bucket(
+            name='bucket-name1', bucket_id='1', task_status=Bucket.TaskStatus.SUCCESS.value,
+            storage_size=111, object_count=1,
+            user_id=None, service_id=None, creation_time=nt
+        )
+        bucket1.save(force_insert=True)
+
+        bucket2 = Bucket(
+            name='bucket-name2', bucket_id='2', task_status=Bucket.TaskStatus.SUCCESS.value,
+            storage_size=222, object_count=2,
+            user_id=None, service_id=None, creation_time=nt - timedelta(days=1)
+        )
+        bucket2.save(force_insert=True)
+
+        bucket3 = Bucket(
+            name='bucket-name3', bucket_id='3', task_status=Bucket.TaskStatus.SUCCESS.value,
+            storage_size=333, object_count=3,
+            user_id=None, service_id=None, creation_time=nt - timedelta(days=6)
+        )
+        bucket3.save(force_insert=True)
+
+        bucket4 = Bucket(
+            name='bucket-name4', bucket_id='4', task_status=Bucket.TaskStatus.SUCCESS.value,
+            storage_size=444, object_count=4,
+            user_id=None, service_id=None, creation_time=nt - timedelta(days=8)
+        )
+        bucket4.save(force_insert=True)
+
+        bucket5 = Bucket(
+            name='bucket-name5', bucket_id='5', task_status=Bucket.TaskStatus.SUCCESS.value,
+            storage_size=555, object_count=5,
+            user_id=None, service_id=None, creation_time=nt - timedelta(days=30)
+        )
+        bucket5.save(force_insert=True)
+
+        bucket6 = Bucket(
+            name='bucket-name6', bucket_id='6', task_status=Bucket.TaskStatus.SUCCESS.value,
+            storage_size=666, object_count=6,
+            user_id=None, service_id=None, creation_time=nt - timedelta(days=60)
+        )
+        bucket6.save(force_insert=True)
+
+        # all
+        r = self.client.get(url)
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.data['current_bucket_count'], 6)
+        self.assertEqual(r.data['new_bucket_count'], 6)
+        self.assertEqual(r.data['new_bucket_delete_count'], 0)
+        self.assertEqual(r.data['total_storage_size'], 111 + 222 + 333 + 444 + 555 + 666)
+        self.assertEqual(r.data['total_object_count'], 1 + 2 + 3 + 4 + 5 + 6)
+
+        # query "time_start" "time_end"
+        query = parse.urlencode(query={
+            'time_start': (nt - timedelta(days=10)).isoformat()[0:19] + 'Z'
+        })
+        r = self.client.get(f'{url}?{query}')
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.data['current_bucket_count'], 6)
+        self.assertEqual(r.data['new_bucket_count'], 4)
+        self.assertEqual(r.data['new_bucket_delete_count'], 0)
+        self.assertEqual(r.data['total_storage_size'], 111 + 222 + 333 + 444)
+        self.assertEqual(r.data['total_object_count'], 1 + 2 + 3 + 4)
+
+        query = parse.urlencode(query={
+            'time_start': (nt - timedelta(days=10)).isoformat()[0:19] + 'Z',
+            'time_end': (nt - timedelta(days=2)).isoformat()[0:19] + 'Z'
+        })
+        r = self.client.get(f'{url}?{query}')
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.data['current_bucket_count'], 6)
+        self.assertEqual(r.data['new_bucket_count'], 2)
+        self.assertEqual(r.data['new_bucket_delete_count'], 0)
+        self.assertEqual(r.data['total_storage_size'], 333 + 444)
+        self.assertEqual(r.data['total_object_count'], 3 + 4)
+
+        bucket3.do_archive(archiver='test')
+        bucket5.do_archive(archiver='test')
+
+        # all
+        r = self.client.get(url)
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.data['current_bucket_count'], 4)
+        self.assertEqual(r.data['new_bucket_count'], 4)
+        self.assertEqual(r.data['new_bucket_delete_count'], 2)
+        self.assertEqual(r.data['total_storage_size'], 111 + 222 + 444 + 666)
+        self.assertEqual(r.data['total_object_count'], 1 + 2 + 4 + 6)
+
+        # query "time_start" "time_end"
+        query = parse.urlencode(query={
+            'time_start': (nt - timedelta(days=10)).isoformat()[0:19] + 'Z'
+        })
+        r = self.client.get(f'{url}?{query}')
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.data['current_bucket_count'], 4)
+        self.assertEqual(r.data['new_bucket_count'], 3)  # bucket 1 2 4
+        self.assertEqual(r.data['new_bucket_delete_count'], 1)  # bucket 3
+        self.assertEqual(r.data['total_storage_size'], 111 + 222 + 444)
+        self.assertEqual(r.data['total_object_count'], 1 + 2 + 4)
+
+        query = parse.urlencode(query={
+            'time_start': (nt - timedelta(days=10)).isoformat()[0:19] + 'Z',
+            'time_end': (nt - timedelta(days=2)).isoformat()[0:19] + 'Z'
+        })
+        r = self.client.get(f'{url}?{query}')
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.data['current_bucket_count'], 4)
+        self.assertEqual(r.data['new_bucket_count'], 1)  # bucket 4
+        self.assertEqual(r.data['new_bucket_delete_count'], 1)  # bucket 3
+        self.assertEqual(r.data['total_storage_size'], 444)
+        self.assertEqual(r.data['total_object_count'], 4)
