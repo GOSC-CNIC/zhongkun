9ec6b081b5d028ae872a4c18a0161b62bce499a4
shun <869588058@qq.com>
2023-05-15 14:42:34 +0800

    后台server修改页面字段布局优化，增加明文输入修改默认登录密码的功能

--- servers/admin.py
+++ servers/admin.py
@@ -1,12 +1,27 @@
 from django.contrib import admin
 from django.utils.translation import gettext_lazy as _
+from django import forms
 
 from utils.model import NoDeleteSelectModelAdmin
 from .models import Server, Flavor, ServerArchive
 
 
+class ServerAdminForm(forms.ModelForm):
+    change_password = forms.CharField(
+        label=_('更改默认登录密码输入'), required=False, min_length=3, max_length=32,
+        help_text=_('如果要更改默认登录密码，请在此输入新密码, 不修改请保持为空'))
+
+    def save(self, commit=True):
+        change_password = self.cleaned_data.get('change_password')      # 如果输入新密码则更改
+        if change_password:
+            self.instance.raw_default_password = change_password
+
+        return super().save(commit=commit)
+
+
 @admin.register(Server)
 class ServerAdmin(NoDeleteSelectModelAdmin):
+    form = ServerAdminForm
     list_display_links = ('id',)
     list_display = ('id', 'service', 'azone_id', 'instance_id', 'vcpus', 'ram', 'disk_size', 'ipv4', 'image',
                     'creation_time', 'start_time', 'user', 'task_status', 'center_quota',
@@ -16,6 +31,16 @@ class ServerAdmin(NoDeleteSelectModelAdmin):
     list_filter = ['service__data_center', 'service', 'classification']
     raw_id_fields = ('user', )
     list_select_related = ('service', 'user', 'vo')
+    readonly_fields = ['default_password']
+
+    fieldsets = [
+        (_('基础信息'), {'fields': ('service', 'azone_id', 'instance_id', 'remarks', 'center_quota')}),
+        (_('配置信息'), {'fields': ('vcpus', 'ram', 'disk_size', 'ipv4', 'image')}),
+        (_('默认登录密码'), {'fields': ('default_user', 'default_password', 'change_password')}),
+        (_('创建和归属信息'), {'fields': ('creation_time', 'task_status', 'classification', 'user', 'vo')}),
+        (_('计量和管控信息'), {'fields': (
+            'pay_type', 'start_time', 'expiration_time', 'lock', 'situation', 'situation_time')}),
+    ]
 
     @admin.display(
         description=_('默认登录密码')
