8d954bb5e07bb39dfcc25b2615f3eb6b4743a1fa
shun <869588058@qq.com>
2023-10-10 11:02:07 +0800

    add ListStorageServiceAdmin api and testcase

--- api/apiviews/storage_views.py
+++ api/apiviews/storage_views.py
@@ -3,6 +3,7 @@ from django.db.models import Sum, Count
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.serializers import Serializer
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 
@@ -102,11 +103,98 @@ class ObjectsServiceViewSet(StorageGenericViewSet):
         except Exception as exc:
             return self.exception_response(exc)
 
+    @swagger_auto_schema(
+        operation_summary=gettext_lazy('列举用户有管理权限的对象存储服务单元'),
+        manual_parameters=[
+            openapi.Parameter(
+                name='center_id',
+                in_=openapi.IN_QUERY,
+                type=openapi.TYPE_STRING,
+                required=False,
+                description='联邦成员机构id'
+            ),
+            openapi.Parameter(
+                name='status',
+                in_=openapi.IN_QUERY,
+                type=openapi.TYPE_STRING,
+                required=False,
+                description=f'查询条件，服务单元服务状态, {ObjectsService.Status.choices}'
+            )
+        ],
+        responses={
+            200: ''
+        }
+    )
+    @action(methods=['GET'], detail=False, url_path='admin', url_name='admin-list')
+    def admin_list(self, request, *args, **kwargs):
+        """
+        列举用户有管理权限的对象存储服务单元
+
+            http code 200：
+            {
+              "count": 1,
+              "next": null,
+              "previous": null,
+              "results": [
+                {
+                  "id": "4fa94896-29a6-11ed-861f-c8009fe2ebbc",
+                  "name": "test iharbor",
+                  "name_en": "en iharbor",
+                  "service_type": "iharbor",
+                  "endpoint_url": "http://159.226.235.188:8001",
+                  "add_time": "2022-09-01T03:29:39.620469Z",
+                  "status": "enable",   # enable：服务中；disable：停止服务；deleted：删除
+                  "remarks": "test",
+                  "provide_ftp": true,
+                  "ftp_domains": ["127.0.0.1", "0.0.0.0"],
+                  "longitude": 0,
+                  "latitude": 0,
+                  "pay_app_service_id": "ss",
+                  "sort_weight": 8,
+                  "data_center": {
+                    "id": "1",
+                    "name": "网络中心",
+                    "name_en": "cnic",
+                    "sort_weight": -1
+                  },
+                  "loki_tag": "xxx"
+                }
+              ]
+            }
+        """
+        center_id = request.query_params.get('center_id', None)
+        status = request.query_params.get('status', None)
+
+        if status is not None and status not in ObjectsService.Status.values:
+            return self.exception_response(
+                exc=errors.InvalidArgument(message=_('参数“status”的值无效'), code='InvalidStatus'))
+
+        qs = ObjectsServiceManager.get_all_has_perm_service(user=request.user)
+        if center_id:
+            qs = qs.filter(data_center_id=center_id)
+
+        if status:
+            qs = qs.filter(status=status)
+
+        qs = qs.order_by('sort_weight')
+        try:
+            services = self.paginate_queryset(queryset=qs)
+            serializer = self.get_serializer(services, many=True)
+            return self.get_paginated_response(data=serializer.data)
+        except Exception as exc:
+            return self.exception_response(exc)
+
     def get_serializer_class(self):
-        if self.action == 'list':
+        if self.action in ['list', 'admin_list']:
             return storage_serializers.ObjectsServiceSerializer
 
         return Serializer
+
+    def get_permissions(self):
+        if self.action == 'admin_list':
+            return [IsAuthenticated()]
+
+        return super().get_permissions()
 
 
 class StorageStatisticsViewSet(StorageGenericViewSet):
--- api/tests/test_storage.py
+++ api/tests/test_storage.py
@@ -64,6 +64,62 @@ class ObjectsServiceTests(MyAPITestCase):
         self.assertEqual(r.data['count'], 1)
         self.assertEqual(len(r.data['results']), 1)
 
+    def test_admin_list_service(self):
+        url = reverse('api:storage-service-admin-list')
+        r = self.client.get(url)
+        self.assertErrorResponse(status_code=401, code='NotAuthenticated', response=r)
+
+        self.client.force_login(self.user)
+        r = self.client.get(url)
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.data['count'], 0)
+        self.assertEqual(len(r.data['results']), 0)
+
+        # set admin
+        self.service.users.add(self.user)
+        r = self.client.get(url)
+        self.assertEqual(r.status_code, 200)
+        self.assertKeysIn(keys=['count', 'next', 'previous', 'results'], container=r.data)
+        self.assertEqual(r.data['count'], 1)
+        self.assertKeysIn(keys=[
+            'id', 'name', 'name_en', 'service_type', 'endpoint_url', 'add_time', 'status', 'remarks', 'provide_ftp',
+            'ftp_domains', 'longitude', 'latitude', 'pay_app_service_id', 'data_center', 'sort_weight', 'loki_tag'
+        ], container=r.data['results'][0])
+        self.assertKeysIn(keys=['id', 'name', 'name_en', 'sort_weight'], container=r.data['results'][0]['data_center'])
+        self.assertIsInstance(r.data['results'][0]['ftp_domains'], list)
+
+        # query 'center_id'
+        url = reverse('api:storage-service-list')
+        query = parse.urlencode(query={'center_id': 'test'})
+        r = self.client.get(f'{url}?{query}')
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.data['count'], 0)
+        self.assertEqual(len(r.data['results']), 0)
+
+        query = parse.urlencode(query={'center_id': self.service.data_center_id})
+        r = self.client.get(f'{url}?{query}')
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.data['count'], 1)
+        self.assertEqual(len(r.data['results']), 1)
+
+        # query 'status'
+        url = reverse('api:storage-service-list')
+        query = parse.urlencode(query={'status': 'sdisable'})
+        r = self.client.get(f'{url}?{query}')
+        self.assertErrorResponse(status_code=400, code='InvalidStatus', response=r)
+
+        query = parse.urlencode(query={'status': 'disable'})
+        r = self.client.get(f'{url}?{query}')
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.data['count'], 0)
+        self.assertEqual(len(r.data['results']), 0)
+
+        query = parse.urlencode(query={'status': 'enable'})
+        r = self.client.get(f'{url}?{query}')
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.data['count'], 1)
+        self.assertEqual(len(r.data['results']), 1)
+
 
 class StorageStatisticsTests(MyAPITestCase):
     def setUp(self):
