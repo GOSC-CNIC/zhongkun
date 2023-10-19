77d473bfeefef5d482da4e905e267005956203fb
shun <869588058@qq.com>
2023-10-19 13:43:57 +0800

    自定义的MyTestCase移动到utils.test.py

--- api/tests/__init__.py
+++ api/tests/__init__.py
@@ -1,46 +1,4 @@
-from rest_framework.test import APITestCase, APITransactionTestCase
-
-from utils.test import get_or_create_user
-
-
-class MyAPITestCase(APITestCase):
-    def assertKeysIn(self, keys: list, container):
-        for k in keys:
-            self.assertIn(k, container)
-
-    def assertErrorResponse(self, status_code: int, code: str, response):
-        self.assertEqual(response.status_code, status_code)
-        self.assertKeysIn(['code', 'message'], response.data)
-        self.assertEqual(response.data['code'], code)
-
-    def assert_is_subdict_of(self, sub: dict, d: dict):
-        for k, v in sub.items():
-            if k in d and v == d[k]:
-                continue
-            else:
-                self.fail(f'{sub} is not sub dict of {d}, Not Equal key is {k}, {v} != {d.get(k)}')
-
-        return True
-
-
-class MyAPITransactionTestCase(APITransactionTestCase):
-    def assertKeysIn(self, keys: list, container):
-        for k in keys:
-            self.assertIn(k, container)
-
-    def assertErrorResponse(self, status_code: int, code: str, response):
-        self.assertEqual(response.status_code, status_code)
-        self.assertKeysIn(['code', 'message'], response.data)
-        self.assertEqual(response.data['code'], code)
-
-    def assert_is_subdict_of(self, sub: dict, d: dict):
-        for k, v in sub.items():
-            if k in d and v == d[k]:
-                continue
-            else:
-                self.fail(f'{sub} is not sub dict of {d}, Not Equal key is {k}, {v} != {d.get(k)}')
-
-        return True
+from utils.test import get_or_create_user, MyAPITestCase, MyAPITransactionTestCase  # 不能删除，其他模块会从此导入
 
 
 def set_auth_header(test_case):
