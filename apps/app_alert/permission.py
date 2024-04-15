00906b4b152a24ffc4b2c555a587daf218ada03d
wenhao <whpang@cnic.cn>
2024-04-15 10:08:33 +0800

    修改告警模块鉴权功能

--- apps/app_alert/permission.py
+++ apps/app_alert/permission.py
@@ -1,7 +1,7 @@
 from rest_framework.permissions import IsAdminUser
 from rest_framework.permissions import BasePermission
-import socket
 from apps.app_alert.models import AlertWhiteListModel
+from utils.iprestrict import IPRestrictor
 
 
 class ReceiverPermission(BasePermission):
@@ -10,16 +10,6 @@ class ReceiverPermission(BasePermission):
     """
 
     def has_permission(self, request, view):
-        request_ip = self.get_remote_ip(request)
-        if AlertWhiteListModel.objects.filter(ip=request_ip).first():
+        client_ip, proxy_ips = IPRestrictor().get_remote_ip(request)
+        if AlertWhiteListModel.objects.filter(ip=client_ip).first():
             return True
-
-    @staticmethod
-    def get_remote_ip(request):
-        host_name = socket.gethostbyname(socket.gethostname())
-        remote_addr = request.META['REMOTE_ADDR']
-        http_x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
-        if remote_addr == host_name:
-            return http_x_forwarded_for
-        else:
-            return remote_addr
