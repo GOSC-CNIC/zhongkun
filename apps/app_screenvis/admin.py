470b882d8c8bf04b516c52bc11083106ffac51ee
shun <869588058@qq.com>
2024-04-18 10:42:28 +0800

    大屏展示，定义云主机和对象存储服务单元、服务单元时序统计数据模型

--- apps/app_screenvis/admin.py
+++ apps/app_screenvis/admin.py
@@ -1,13 +1,16 @@
 from datetime import datetime
 
+from django import forms
 from django.contrib import admin
 from django.utils.translation import gettext_lazy
 from django.utils import timezone as dj_timezone
+from django.utils.html import format_html
+
 
 from utils.model import BaseModelAdmin
 from .models import (
     ScreenConfig, DataCenter, MetricMonitorUnit, LogMonitorUnit, HostCpuUsage,
-    ApiAllowIP
+    ApiAllowIP, ServerService, ObjectService, ServerServiceTimedStats, ObjectServiceTimedStats, VPNTimedStats
 )
 
 
@@ -104,3 +107,124 @@ class HostCpuUsageAdmin(BaseModelAdmin):
 class ApiAllowIPAdmin(BaseModelAdmin):
     list_display = ('id', 'ip_value', 'creation_time', 'update_time', 'remark')
     list_display_links = ('id',)
+
+
+class ServiceForm(forms.ModelForm):
+    change_password = forms.CharField(
+        label=gettext_lazy('更改用户密码输入'), required=False, min_length=6, max_length=32,
+        help_text=gettext_lazy('如果要更改服务认证用户密码，请在此输入新密码, 不修改请保持为空'))
+
+    class Meta:
+        widgets = {
+            'remarks': forms.Textarea(attrs={'cols': 80, 'rows': 6}),
+            "password": forms.PasswordInput(
+                attrs={'placeholder': '********', 'autocomplete': 'off', 'data-toggle': 'password'}),
+        }
+
+    def save(self, commit=True):
+        change_password = self.cleaned_data.get('change_password')      # 如果输入新密码则更改
+        if change_password:
+            self.instance.set_password(change_password)
+
+        return super().save(commit=commit)
+
+
+@admin.register(ServerService)
+class ServerServiceAdmin(BaseModelAdmin):
+    form = ServiceForm
+    list_display_links = ('id',)
+    list_display = ('id', 'name', 'name_en', 'data_center', 'sort_weight', 'endpoint_url',
+                    'username', 'password', 'raw_password', 'creation_time', 'remarks')
+    search_fields = ['name', 'name_en', 'endpoint_url', 'remarks']
+    list_filter = ['data_center']
+    list_select_related = ('data_center',)
+    raw_id_fields = ('data_center',)
+    list_editable = ('sort_weight',)
+
+    readonly_fields = ('password',)
+
+    @admin.display(description=gettext_lazy("原始密码"))
+    def raw_password(self, obj):
+        passwd = obj.raw_password()
+        if not passwd:
+            return passwd
+
+        return format_html(f'<div title="{passwd}">******</div>')
+
+
+@admin.register(ObjectService)
+class ObjectServiceAdmin(BaseModelAdmin):
+    form = ServiceForm
+    list_display_links = ('id',)
+    list_display = ('id', 'name', 'name_en', 'data_center', 'sort_weight', 'endpoint_url',
+                    'username', 'password', 'raw_password', 'creation_time', 'remarks')
+    search_fields = ['name', 'name_en', 'endpoint_url', 'remarks']
+    list_filter = ['data_center']
+    list_select_related = ('data_center',)
+    raw_id_fields = ('data_center',)
+    list_editable = ('sort_weight',)
+
+    readonly_fields = ('password',)
+
+    @admin.display(description=gettext_lazy("原始密码"))
+    def raw_password(self, obj):
+        passwd = obj.raw_password()
+        if not passwd:
+            return passwd
+
+        return format_html(f'<div title="{passwd}">******</div>')
+
+
+@admin.register(ServerServiceTimedStats)
+class ServerServiceTimedStatsAdmin(BaseModelAdmin):
+    list_display_links = ('id',)
+    list_display = ('id', 'service', 'show_time', 'server_count', 'disk_count', 'ip_count',
+                    'ip_used_count', 'mem_size', 'mem_used_size')
+    list_filter = ['service']
+    list_select_related = ('service',)
+    raw_id_fields = ('service',)
+
+    @admin.display(description=gettext_lazy("统计时间"))
+    def show_time(self, obj):
+        try:
+            dt = datetime.fromtimestamp(obj.timestamp, tz=dj_timezone.get_default_timezone())
+        except Exception as exc:
+            return ''
+
+        return dt.isoformat(sep=' ')
+
+
+@admin.register(VPNTimedStats)
+class VPNTimedStatsAdmin(BaseModelAdmin):
+    list_display_links = ('id',)
+    list_display = ('id', 'service', 'show_time', 'vpn_online_count', 'vpn_active_count', 'vpn_count')
+    list_filter = ['service']
+    list_select_related = ('service',)
+    raw_id_fields = ('service',)
+
+    @admin.display(description=gettext_lazy("统计时间"))
+    def show_time(self, obj):
+        try:
+            dt = datetime.fromtimestamp(obj.timestamp, tz=dj_timezone.get_default_timezone())
+        except Exception as exc:
+            return ''
+
+        return dt.isoformat(sep=' ')
+
+
+@admin.register(ObjectServiceTimedStats)
+class ObjectServiceTimedStatsAdmin(BaseModelAdmin):
+    list_display_links = ('id',)
+    list_display = ('id', 'service', 'show_time', 'bucket_count', 'bucket_storage', 'storage_used', 'storage_capacity')
+    list_filter = ['service']
+    list_select_related = ('service',)
+    raw_id_fields = ('service',)
+
+    @admin.display(description=gettext_lazy("统计时间"))
+    def show_time(self, obj):
+        try:
+            dt = datetime.fromtimestamp(obj.timestamp, tz=dj_timezone.get_default_timezone())
+        except Exception as exc:
+            return ''
+
+        return dt.isoformat(sep=' ')
--- apps/app_screenvis/models.py
+++ apps/app_screenvis/models.py
@@ -7,7 +7,7 @@ from django.utils import timezone as dj_timezone
 from django.core.exceptions import ValidationError
 from django.core.validators import URLValidator
 
-from utils.model import UuidModel
+from utils.model import UuidModel, get_encryptor
 from utils.iprestrict import convert_iprange
 
 
@@ -206,3 +206,164 @@ class ApiAllowIP(models.Model):
 
         if isinstance(subnet, ipaddress.IPv4Network):
             self.ip_value = str(subnet)
+
+
+class BaseService(models.Model):
+    class Status(models.TextChoices):
+        ENABLE = 'enable', _('服务中')
+        DISABLE = 'disable', _('暂停服务')
+        DELETED = 'deleted', _('删除')
+
+    id = models.BigAutoField(primary_key=True)
+    data_center = models.ForeignKey(
+        to=DataCenter, null=True, on_delete=models.SET_NULL, related_name='+', verbose_name=_('数据中心'),
+        db_constraint=False, blank=True, default=None)
+    name = models.CharField(max_length=255, verbose_name=_('服务名称'))
+    name_en = models.CharField(verbose_name=_('服务英文名称'), max_length=255, default='')
+    endpoint_url = models.CharField(
+        max_length=255, verbose_name=_('服务地址url'), help_text='http(s)://{hostname}:{port}/')
+    username = models.CharField(max_length=128, verbose_name=_('用户名'), help_text=_('用于此服务认证的用户名'))
+    password = models.CharField(max_length=255, verbose_name=_('密码'))
+    status = models.CharField(
+        verbose_name=_('服务状态'), max_length=32, choices=Status.choices, default=Status.ENABLE.value)
+    creation_time = models.DateTimeField(verbose_name=_('创建时间'), auto_now_add=True)
+    remarks = models.CharField(max_length=255, default='', blank=True, verbose_name=_('备注'))
+    sort_weight = models.IntegerField(verbose_name=_('排序值'), default=0, help_text=_('值越小排序越靠前'))
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+    def clean(self):
+        # 网址验证
+        try:
+            URLValidator(schemes=['http', 'https'])(self.endpoint_url)
+        except ValidationError:
+            raise ValidationError(message={'endpoint_url': gettext('不是一个有效的网址')})
+
+    def raw_password(self):
+        """
+        :return:
+            str     # success
+            None    # failed, invalid encrypted password
+        """
+        encryptor = get_encryptor()
+        try:
+            return encryptor.decrypt(self.password)
+        except encryptor.InvalidEncrypted as e:
+            return None
+
+    def set_password(self, raw_password: str):
+        encryptor = get_encryptor()
+        self.password = encryptor.encrypt(raw_password)
+
+    def raw_vpn_password(self):
+        """
+        :return:
+            str     # success
+            None    # failed, invalid encrypted password
+        """
+        encryptor = get_encryptor()
+        try:
+            return encryptor.decrypt(self.vpn_password)
+        except encryptor.InvalidEncrypted as e:
+            return None
+
+    def set_vpn_password(self, raw_password: str):
+        encryptor = get_encryptor()
+        self.vpn_password = encryptor.encrypt(raw_password)
+
+
+class ServerService(BaseService):
+    class Meta:
+        db_table = 'screenvis_serverservice'
+        ordering = ['sort_weight']
+        verbose_name = _('云主机服务单元')
+        verbose_name_plural = verbose_name
+
+
+class ObjectService(BaseService):
+    class Meta:
+        db_table = 'screenvis_objectservice'
+        ordering = ['sort_weight']
+        verbose_name = _('对象存储服务单元')
+        verbose_name_plural = verbose_name
+
+
+class BaseTimed(UuidModel):
+    """
+    时序数据基类
+    """
+    timestamp = models.PositiveBigIntegerField(null=False, blank=False, verbose_name="统计时间")
+
+    class Meta:
+        abstract = True
+
+    def clean(self):
+        try:
+            datetime.fromtimestamp(self.timestamp, tz=dj_timezone.get_default_timezone())
+        except Exception as exc:
+            raise ValidationError({'timestamp': f'无效的时间戳，{str(exc)}，当前时间戳为:{int(dj_timezone.now().timestamp())}'})
+
+
+class ServerServiceTimedStats(BaseTimed):
+    service = models.ForeignKey(
+        to=ServerService, verbose_name='服务单元', on_delete=models.DO_NOTHING, null=True, blank=False,
+        db_constraint=False, db_index=False)
+    server_count = models.IntegerField(verbose_name=_('云主机数'), blank=True, default=0)
+    disk_count = models.IntegerField(verbose_name=_('云硬盘数'), blank=True, default=0)
+    ip_count = models.IntegerField(verbose_name=_('IP总数'), blank=True, default=0)
+    ip_used_count = models.IntegerField(verbose_name=_('已用IP数'), blank=True, default=0)
+    mem_size = models.IntegerField(verbose_name=_('内存总数(GiB)'), blank=True, default=0)
+    mem_used_size = models.IntegerField(verbose_name=_('已用IP总数(GiB)'), blank=True, default=0)
+
+    class Meta:
+        db_table = 'screenvis_server_timedstats'
+        ordering = ['-timestamp']
+        verbose_name = _('云主机服务单元时序统计数据')
+        verbose_name_plural = verbose_name
+        indexes = [
+            models.Index(fields=['timestamp'], name='idx_screen_server_tmstats_ts')
+        ]
+
+
+class VPNTimedStats(BaseTimed):
+    service = models.ForeignKey(
+        to=ServerService, verbose_name='服务单元', on_delete=models.DO_NOTHING, null=True, blank=False,
+        db_constraint=False, db_index=False)
+    vpn_online_count = models.IntegerField(verbose_name=_('VPN账户在线数'), blank=True, default=0)
+    vpn_active_count = models.IntegerField(verbose_name=_('VPN有效数'), blank=True, default=0)
+    vpn_count = models.IntegerField(verbose_name=_('VPN总数'), blank=True, default=0)
+
+    class Meta:
+        db_table = 'screenvis_vpn_timedstats'
+        ordering = ['-timestamp']
+        verbose_name = _('VPN时序统计数据')
+        verbose_name_plural = verbose_name
+        indexes = [
+            models.Index(fields=['timestamp'], name='idx_screen_vpn_tmstats_ts')
+        ]
+
+
+class ObjectServiceTimedStats(BaseTimed):
+    service = models.ForeignKey(
+        to=ObjectService, verbose_name='服务单元', on_delete=models.DO_NOTHING, null=True, blank=False,
+        db_constraint=False, db_index=False)
+    bucket_count = models.IntegerField(verbose_name=_('存储桶总数'), blank=True, default=0)
+    bucket_storage = models.BigIntegerField(verbose_name=_('存储桶总数据量'), blank=True, default=0)
+    storage_used = models.BigIntegerField(verbose_name=_('已用容量'), blank=True, default=0)
+    storage_capacity = models.BigIntegerField(verbose_name=_('总容量'), blank=True, default=0)
+
+    class Meta:
+        db_table = 'screenvis_object_timedstats'
+        ordering = ['-timestamp']
+        verbose_name = _('对象存储服务单元时序统计数据')
+        verbose_name_plural = verbose_name
+        indexes = [
+            models.Index(fields=['timestamp'], name='idx_screen_obj_tmstats_ts')
+        ]
+
+# ObjectServiceTimedStats.objects.order_by('service_id', '-timestamp').distinct('service_id')
+# ObjectServiceTimedStats.objects.values('service_id').annotate(last_timestamp=Max('timestamp')).values('service_id', 'last_timestamp')
