6b6a656e7a5ead354c5242b7fe40f34ab7643f64
wenhao <whpang@cnic.cn>
2024-04-30 16:28:57 +0800

    app_alert:修改钉钉告警通知规则。1.首次提醒 ；2.重复提醒：（1）第一次，告警持续20分钟，第二次，告警持续60分钟，每天早上8:00、下午16:00，推送未处理告警信息

--- apps/app_alert/scripts/alert_dingtalk_notification.py
+++ apps/app_alert/scripts/alert_dingtalk_notification.py
@@ -4,6 +4,8 @@
 import re
 import os
 import sys
+import time
+
 from django import setup
 from pathlib import Path
 
@@ -38,45 +40,71 @@ class DingTalk(object):
         self.clusters = ["mail_metric", "mail_log"]
         logger.info(f"当前时间范围：{DateUtils.ts_to_date(self.start)}, {DateUtils.ts_to_date(self.end)}")
         self.instance_property_mapping = self.get_instance_property_mapping()
-        self.second_notice_minute = 10
-        self.second_notice_limit = self.second_notice_minute * 60
+
+    @staticmethod
+    def current_datetime():
+        now = DateUtils.now()
+        week, hour, minute = now.isoweekday(), now.hour, now.minute
+        return week, hour, minute
 
     def run(self):
+        """
+        首次提醒
+        重复提醒
+            1.第一次，告警持续20分钟
+            2.第二次，告警持续60分钟
+            3.每天早上8:00、下午16:00，推送未处理告警信息
+        """
+        week, hour, minute = self.current_datetime()
+        if hour in [8, 16] and minute == 1:
+            self.specific_time_run()
+        else:
+            self.daily_task_run()
+
+    def daily_task_run(self):
+        """
+        :return:
+        """
         self.metric_notification()
         self.log_notification()
         self.work_order_notification()
-        self.firing_second_notice()
+        self.repeat_notice(minute=20)  # 持续20分钟还没有恢复的，再次通知
+        self.repeat_notice(minute=60)  # 持续60分钟还没有恢复的，再次通知
 
-    def firing_second_notice(self):
-        """
-        """
+    def specific_time_run(self):
+        self.work_order_notification()
+        # 指标类自动恢复告警
+        metric_resolved_alerts = self.search_resolved_alerts(alert_type="metric")
+        title, record = self.metric_alert_format(metric_resolved_alerts, "Resolved")
+        if record:
+            self.post(title, record)
         # 指标类长时间未解决 重复通知
-        for alert in self.search_firing_alerts(
-                alert_type="metric",
-                start=self.start - self.second_notice_limit,
-                end=self.end - self.second_notice_limit):
-            if not self.alert_cluster_filter(alert):
-                continue
-            if self.has_created_work_order(alert):
-                logger.info("该自动恢复的指标告警已经创建工单")
-                continue
-            title, record = self.metric_alert_format(alert, "Firing", second=True)
-            if record:
-                self.post(title, record)
+        metric_firing_alerts = self.search_firing_alerts(alert_type="metric", start=0, end=self.end)
+        title, record = self.metric_alert_format(metric_firing_alerts, "Firing", minute=-1)
+        if record:
+            self.post(title, record)
+        # 日志类长时间未解决 重复通知
+        log_alerts = self.search_firing_alerts(alert_type="log", start=0, end=self.end)
+        title, record = self.log_alert_format(log_alerts, minute=-1)
+        if record:
+            self.post(title, record)
+
+    def repeat_notice(self, minute):
+        # 指标类长时间未解决 重复通知
+        metric_alerts = self.search_firing_alerts(
+            alert_type="metric",
+            start=self.start - minute * 60,
+            end=self.end - minute * 60)
+        title, record = self.metric_alert_format(metric_alerts, "Firing", minute=minute)
+        if record:
+            self.post(title, record)
 
         # 日志类长时间未解决 重复通知
-        firing_log_alerts = []
-        for alert in self.search_firing_alerts(
-                alert_type="log",
-                start=self.start - self.second_notice_limit,
-                end=self.end - self.second_notice_limit):
-            if not self.alert_cluster_filter(alert):
-                continue
-            if self.has_created_work_order(alert):
-                logger.info("该自动恢复的指标告警已经创建工单")
-                continue
-            firing_log_alerts.append(alert)
-        title, record = self.log_alert_format(firing_log_alerts, second=True)
+        log_alerts = self.search_firing_alerts(
+            alert_type="log",
+            start=self.start - minute * 60,
+            end=self.end - minute * 60)
+        title, record = self.log_alert_format(log_alerts, minute=minute)
         if record:
             self.post(title, record)
 
@@ -93,13 +121,30 @@ class DingTalk(object):
         with MysqlManager() as client:
             sql = f'select * from alert_firing where ' \
                   f'{start} < creation and creation <= {end} and type="{alert_type}";'
-            return client.search(sql)
+            alerts = []
+            result = client.search(sql)
+            for alert in result:
+                if not self.alert_cluster_filter(alert):
+                    continue
+                if self.has_created_work_order(alert):
+                    continue
+                alerts.append(alert)
+            return alerts
 
     def search_resolved_alerts(self, alert_type):
         with MysqlManager() as client:
             sql = f'select * from alert_resolved where ' \
                   f'{self.start} < modification and modification <= {self.end} and type="{alert_type}";'
-            return client.search(sql)
+            result = client.search(sql)
+            alerts = []
+            for alert in result:
+                if not self.alert_cluster_filter(alert):
+                    continue
+                if self.has_created_work_order(alert):
+                    logger.info("该自动恢复的指标告警已经创建工单")
+                    continue
+                alerts.append(alert)
+            return alerts
 
     @staticmethod
     def has_created_work_order(alert):
@@ -143,41 +188,39 @@ class DingTalk(object):
         logger.info(str(ret))
 
     def metric_notification(self):
-        logger.info("指标类通知")
+        """
+        指标类告警通知
+        :return:
+        """
+        logger.info("指标告警通知")
         # 新触发告警
-        for alert in self.search_firing_alerts(alert_type="metric", start=self.start, end=self.end):
-            if not self.alert_cluster_filter(alert):
-                continue
-            title, record = self.metric_alert_format(alert, "Firing")
-            if record:
-                self.post(title, record)
+        metric_alerts = self.search_firing_alerts(alert_type="metric", start=self.start, end=self.end)
+        title, record = self.metric_alert_format(metric_alerts, "Firing")
+        if record:
+            self.post(title, record)
 
         # 自动恢复告警
-        for alert in self.search_resolved_alerts(alert_type="metric"):
-            if not self.alert_cluster_filter(alert):
-                continue
-            if self.has_created_work_order(alert):
-                logger.info("该自动恢复的指标告警已经创建工单")
-                continue
-            title, record = self.metric_alert_format(alert, "Resolved")
-            if record:
-                self.post(title, record)
+        resolved_alerts = self.search_resolved_alerts(alert_type="metric")
+        title, record = self.metric_alert_format(resolved_alerts, "Resolved")
+        if record:
+            self.post(title, record)
 
     def log_notification(self):
-        logger.info("日志类通知")
-        firing_log_alerts = []
-        for alert in self.search_firing_alerts(alert_type="log", start=self.start, end=self.end):
-            if not self.alert_cluster_filter(alert):
-                continue
-            if self.has_created_work_order(alert):
-                logger.info("该日志类告警已经创建工单")
-                continue
-            firing_log_alerts.append(alert)
-        title, record = self.log_alert_format(firing_log_alerts)
+        """
+        日志类告警通知
+        :return:
+        """
+        logger.info("日志类告警通知")
+        log_alerts = self.search_firing_alerts(alert_type="log", start=self.start, end=self.end)
+        title, record = self.log_alert_format(log_alerts)
         if record:
             self.post(title, record)
 
     def work_order_notification(self):
+        """
+        告警工单通知
+        :return:
+        """
         alerts = self.search_work_order()
         if not alerts:
             return
@@ -204,9 +247,6 @@ class DingTalk(object):
         instance = alert.get("instance") or ""
         return instance
 
-    def parse_log_description(self, alert):
-        return alert.get("description")
-
     def parser_property_info(self, instance):
         instance_property = self.instance_property_mapping.get(instance) or {}
         property_id = "**设备序号**: {}".format(instance_property.get("id") or "")
@@ -214,57 +254,25 @@ class DingTalk(object):
         property_director = "**管理员**: {}".format(instance_property.get("director") or "")
         return property_id, property_name, property_director
 
-    def metric_alert_format(self, alert, status, second=False):
-        if not alert:
-            return '', ''
-        instance = self.parse_alert_instance(alert)
-        property_id, property_name, property_director = self.parser_property_info(instance)
-        alert_status = "**告警状态**: {}".format(status)
-        start = "**告警时间**: {}".format(DateUtils.ts_to_date(alert.get("start")))
-        # alert_type = "**告警类别**: {}".format(alert.get("type"))
-        alert_name = "**告警名称**: {}".format(alert.get("name"))
-        # severity = "**告警级别**: {}".format(alert.get("severity"))
-        instance = "**设备IP**: {}".format(instance)
-        _id = f"**告警ID**: {alert.get('id')[:10]}"
-        summary = "**告警摘要**: {}".format(alert.get("summary"))
-        description = "**告警详情**: {}".format(alert.get("description"))
-        record_title = ["---",
-                        f"**指标类告警({status})**",
-                        start, ]
-        if second:
-            record_title.append(f'**Tips: 告警时长超过{self.second_notice_minute}分钟,将不再发送重复提醒**')
-        record_title.append('---')
-
-        record = [
-            property_id,
-            property_name,
-            instance,
-            property_director,
-            alert_name,
-            _id,
-            summary,
-            description,
-            "---"]
-        record = record_title + record
-        record = "\n\n".join(record)
-        return alert.get("name"), record
-
-    def log_alert_format(self, alerts, second=False):
+    def metric_alert_format(self, alerts, status, minute=0):
         if not alerts:
             return '', ''
-        alert_status = "**日志类告警(Firing)**"
+        alert_status = f"### **指标告警({status})**"
         start = "**告警时间**: {}".format(DateUtils.ts_to_date(alerts[0].get("start")))
         alert_msg_mapping = dict()
         for alert in alerts:
             instance = self.parse_alert_instance(alert)
             if not alert_msg_mapping.get(instance):
                 alert_msg_mapping[instance] = []
-            description = self.parse_log_description(alert)
+            description = alert.get("description")
             alert_msg_mapping[instance].append(f"{description}, {alert.get('id')[:10]}")
-        record_msg_list = ["---", alert_status, start]
-        if second:
-            record_msg_list.append(f'**Tips: 告警时长超过{self.second_notice_minute}分钟,将不再发送重复提醒**')
-        record_msg_list.append("---")
+        record_msg_list = list()
+        if minute:
+            if minute == -1:
+                record_msg_list.append(f'### *Tip:未处理告警信息推送*')
+            else:
+                record_msg_list.append(f'### *Tip:告警时长超过{minute}分钟*')
+        record_msg_list.extend(["---", alert_status, start, "---"])
         for instance, descriptions in alert_msg_mapping.items():
             property_id, property_name, property_director = self.parser_property_info(instance)
             instance_field = "**设备IP**: {}".format(instance)
@@ -276,11 +284,42 @@ class DingTalk(object):
                 description = "**告警信息**: {}".format(description)
                 record_msg_list.append(description)
             record_msg_list.append("---\n\n")
+        return f"指标告警：{list(alert_msg_mapping.keys())[0]}", "\n\n".join(record_msg_list)
 
+    def log_alert_format(self, alerts, minute=0):
+        if not alerts:
+            return '', ''
+        alert_status = "### **日志告警(Firing)**"
+        start = "**告警时间**: {}".format(DateUtils.ts_to_date(alerts[0].get("start")))
+        alert_msg_mapping = dict()
+        for alert in alerts:
+            instance = self.parse_alert_instance(alert)
+            if not alert_msg_mapping.get(instance):
+                alert_msg_mapping[instance] = []
+            description = alert.get("description")
+            alert_msg_mapping[instance].append(f"{description}, {alert.get('id')[:10]}")
+        record_msg_list = list()
+        if minute:
+            if minute == -1:
+                record_msg_list.append(f'### *Tip:未处理告警信息推送*')
+            else:
+                record_msg_list.append(f'### *Tip:告警时长超过{minute}分钟*')
+        record_msg_list.extend(["---", alert_status, start, "---"])
+        for instance, descriptions in alert_msg_mapping.items():
+            property_id, property_name, property_director = self.parser_property_info(instance)
+            instance_field = "**设备IP**: {}".format(instance)
+            record_msg_list.append(property_id)
+            record_msg_list.append(property_name)
+            record_msg_list.append(instance_field)
+            record_msg_list.append(property_director)
+            for description in descriptions:
+                description = "**告警信息**: {}".format(description)
+                record_msg_list.append(description)
+            record_msg_list.append("---\n\n")
         return f"日志告警：{list(alert_msg_mapping.keys())[0]}", "\n\n".join(record_msg_list)
 
     def work_order_format(self, collect_id, alerts):
-        alert_status = "**工单处理**"
+        alert_status = "### **工单处理**"
         start_timestamp = alerts[0].get("creation")
         start = "**创建时间**: {}".format(DateUtils.ts_to_date(start_timestamp))
         # 按照主机IP进行分类
@@ -289,7 +328,7 @@ class DingTalk(object):
             instance = self.parse_alert_instance(alert)
             if not alert_msg_mapping.get(instance):
                 alert_msg_mapping[instance] = []
-            description = self.parse_log_description(alert)
+            description = alert.get("description")
             description = f"{description}, {alert.get('id')[:10]}"
             creator = f"{alert.get('last_name')}{alert.get('first_name')}({alert.get('email')})"
             status = alert.get("status")
@@ -343,5 +382,7 @@ def run_task_use_lock():
 
 
 if __name__ == '__main__':
+    # while True:
+    #     run_task_use_lock()
+    #     time.sleep(1)
     run_task_use_lock()
-    # DingTalk().run()
