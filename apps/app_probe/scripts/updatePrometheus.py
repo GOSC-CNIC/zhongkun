8207d9856795dc90a7ce01f66fe3ef3523fd0c07
wanghuang <wanghuangsuper@aliyun.com>
2024-06-18 10:29:19 +0800

    探针脚本更新

--- apps/app_probe/scripts/updatePrometheus.py
+++ apps/app_probe/scripts/updatePrometheus.py
@@ -2,7 +2,6 @@ import sys
 import os
 import requests
 from django import setup
-from django.utils import timezone
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
 # 设置项目的配置文件 不做修改的话就是 settings 文件
@@ -10,6 +9,12 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_site.settings")
 setup()
 
 from apps.app_probe.models import ProbeDetails, ProbeMonitorWebsite
+from apps.app_probe.handlers.handlers import ProbeHandlers
+
+from core.loggers import config_script_logger
+
+
+probe_logger = config_script_logger(name='app-probe-script', filename='app_probe_script.log')
 
 
 def get_website_version(version_url):
@@ -17,7 +22,7 @@ def get_website_version(version_url):
 
     respond = requests.get(version_url)
     if respond.status_code != 200:
-        print(f'{timezone.now()} -- 查询版本信息有误 =》 {respond.text}')
+        probe_logger.error(f'查询版本信息有误:{respond.text}')
         return None
 
     respond = respond.json()
@@ -27,7 +32,7 @@ def get_website_version(version_url):
 def get_website_task(task_list_url):
     respond = requests.get(task_list_url)
     if respond.status_code != 200:
-        print(f'{timezone.now()} -- 查询任务信息有误 =》 {respond.text}')
+        probe_logger.error(f'查询任务信息有误:{respond.text}')
         return None, None, None
     respond = respond.json()
     return respond['results'], respond['has_next'], respond['next_marker']
@@ -50,8 +55,7 @@ def get_task_data(task_list_url, next_task_list_url):
                     url=task['url'],
                 )
             except Exception as e:
-                print(f'{timezone.now()} -- 添加任务 url 信息有误，路径：{next_task_list_url} =》 {str(e)}')
-                pass
+                probe_logger.error(f'添加任务 url 信息有误，路径：{next_task_list_url}；错误信息： {str(e)}')
 
         init_data = init_data + data  # 合并数据
 
@@ -61,43 +65,55 @@ def get_task_data(task_list_url, next_task_list_url):
             return init_data
 
 
-def get_zk_website_task():
+def get_zk_website_task(web_version_url, web_task_list_url):
     """
     一次性将站点信息写到数据库中
     """
-    version_url = "http://servicebackend.cstcloud.cn/api/monitor/website-task/version"
-    next_task_list_url = task_list_url = "http://servicebackend.cstcloud.cn/api/monitor/website-task"
+    version_url = web_version_url
+    next_task_list_url = task_list_url = web_task_list_url
     try:
         version = get_website_version(version_url=version_url)
     except Exception as e:
-        print(f'{timezone.now()} -- 查询版本号出现错误 =》 {str(e)}')
+        probe_logger.error(f'查询版本号出现错误：{str(e)}')
         return
 
     if not version:
-        print(f'{timezone.now()} -- 未找到版本号信息')
+        probe_logger.error(f'未找到版本号信息')
         return
 
     try:
         obj = ProbeDetails().get_instance()
     except Exception as e:
-        print(f'{timezone.now()} -- 查询探针信息有误， 检查是否将探针信息填写完整 =》 {str(e)}')
+        probe_logger.error(f'查询探针信息有误， 检查是否将探针信息填写完整：{str(e)}')
         return
 
     if not obj:
         try:
-            obj = ProbeDetails.objects.create(version=version, probe_type=1)
+            obj = ProbeDetails.objects.create(version=version, probe_name='探针服务')
         except Exception as e:
-            print(f'{timezone.now()} -- 创建默认探针信息有误 =》 {str(e)}')
+            probe_logger.error(f'创建默认探针信息有误：{str(e)}')
             return
 
     if obj.version != version:
         obj.version = version
         obj.save(update_fields=['version'])
 
-    get_task_data(task_list_url=task_list_url, next_task_list_url=next_task_list_url)
+        get_task_data(task_list_url=task_list_url, next_task_list_url=next_task_list_url)
+
+        update_prometheus_config()
 
     return
 
+def update_prometheus_config():
+
+    try:
+        ProbeHandlers().handler_prometheus_config()
+    except Exception as e:
+        probe_logger.error(f'重新加载prometheus服务配置有误：{str(e)}')
+
 
 if __name__ == '__main__':
-    get_zk_website_task()
+    web_version_url = ""
+    web_task_list_url = ""
+    get_zk_website_task(web_version_url, web_task_list_url)
+    # update_prometheus_config()
