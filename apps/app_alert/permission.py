169bbb1bd2cb9294de522fdbd96a599e3f6afdcb
wenhao <whpang@cnic.cn>
2024-06-27 13:00:46 +0800

    告警模块，修改告警接收接口获取客户端ip的方法

--- apps/app_alert/permission.py
+++ apps/app_alert/permission.py
@@ -25,12 +25,32 @@ class AlertAPIIPRestrictor(IPRestrictor):
 
     @staticmethod
     def get_remote_ip(request):
-        remote_addr = request.META['REMOTE_ADDR']
-        http_x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
-        if hash_md5(remote_addr) == '3aec3006463da42169cc870cfecc052b':
-            return http_x_forwarded_for
+        """
+        获取客户端的ip地址和代理ip
+
+            X-Forwarded-For 可能伪造，需要在服务一级代理防范处理
+            比如nginx：
+            uwsgi_param X-Forwarded-For $remote_addr;     不能使用 $proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-For $remote_addr;     不能使用 $proxy_add_x_forwarded_for;
+
+        :return: (
+            str,    # 客户端真实ip地址
+            list    # 经过的代理ip地址列表
+        )
+        """
+        if 'X-Forwarded-For' in request.META:
+            h = request.META.get('X-Forwarded-For')
+        elif 'HTTP_X-Forwarded-For' in request.META:
+            h = request.META.get('HTTP_X-Forwarded-For')
         else:
-            return remote_addr
+            # 标头 X-Forwarded-For 不存在
+            # 没有经过代理时， REMOTE_ADDR是客户端地址
+            # 经过代理时，socket方式时， REMOTE_ADDR是客户端地址；http方式时，REMOTE_ADDR是代理地址（如果代理到本机，获取的ip可能是127.0.0.1）
+            return request.META.get('REMOTE_ADDR', ''), []
+
+        ips = h.split(',')
+        ips = [i.strip(' ') for i in ips]
+        return ips.pop(0), ips
 
     def check_restricted(self, request):
         """
@@ -38,7 +58,7 @@ class AlertAPIIPRestrictor(IPRestrictor):
             ip: str
         :raises: AccessDenied
         """
-        client_ip = self.get_remote_ip(request)
+        client_ip, proxy_ips = self.get_remote_ip(request)
         self.is_restricted(client_ip=client_ip)
         return client_ip
 
