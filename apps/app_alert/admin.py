58842bf8c00f463b48f1a67d091206285b89153b
yushunwang <869588058@qq.com>
2024-06-28 11:34:34 +0800

    告警，注册告警模型到admin后台

--- apps/app_alert/admin.py
+++ apps/app_alert/admin.py
@@ -1,9 +1,24 @@
+from typing import Union
+from datetime import datetime
+
 from django.contrib import admin
-from apps.app_alert.models import AlertMonitorJobServer
+from django.utils.translation import gettext_lazy
+from django.utils import timezone as dj_timezone
+
 from utils.model import BaseModelAdmin
+from apps.app_alert.models import AlertMonitorJobServer, ResolvedAlertModel, AlertModel
 
 
-# Register your models here.
+def ts_to_time_dispaly(ts: Union[int, float]) -> str:
+    if ts is None:
+        return ''
+
+    try:
+        dt = datetime.fromtimestamp(ts, tz=dj_timezone.get_default_timezone())
+    except Exception as exc:
+        return str(ts)
+
+    return dt.isoformat(sep=' ')
 
 
 @admin.register(AlertMonitorJobServer)
@@ -15,10 +30,53 @@ class AlertMonitorJobServerAdmin(BaseModelAdmin):
         'remark', ]
     list_display_links = (
         'id',
-        'name',
-        'sort_weight',
-        'remark',)
+        'name',)
 
     filter_horizontal = (
         "users",
     )
+
+
+class AlertAdminBase(BaseModelAdmin):
+    list_display = ['id', 'name', 'type', 'status', 'severity',
+                    'start', 'show_start', 'end', 'show_end', 'show_recovery', 'count',
+                    'instance', 'port', 'cluster', 'fingerprint',
+                    'show_creation', 'show_first_notification', 'show_last_notification', 'order_id',
+                    'summary', 'description'
+                    ]
+    list_display_links = ('id', 'name')
+    search_fields = ('summary', 'description', 'instance', 'cluster')
+
+    @admin.display(description=gettext_lazy("开始时间"))
+    def show_start(self, obj):
+        return ts_to_time_dispaly(obj.start)
+
+    @admin.display(description=gettext_lazy("结束时间"))
+    def show_end(self, obj):
+        return ts_to_time_dispaly(obj.end)
+
+    @admin.display(description=gettext_lazy("恢复时间"))
+    def show_recovery(self, obj):
+        return ts_to_time_dispaly(obj.recovery)
+
+    @admin.display(description=gettext_lazy("创建时间"))
+    def show_creation(self, obj):
+        return ts_to_time_dispaly(obj.creation)
+
+    @admin.display(description=gettext_lazy("首次通知时间"))
+    def show_first_notification(self, obj):
+        return ts_to_time_dispaly(obj.first_notification)
+
+    @admin.display(description=gettext_lazy("上次通知时间"))
+    def show_last_notification(self, obj):
+        return ts_to_time_dispaly(obj.last_notification)
+
+
+@admin.register(AlertModel)
+class AlertModelAdmin(AlertAdminBase):
+    pass
+
+
+@admin.register(ResolvedAlertModel)
+class ResolvedAlertModelAdmin(AlertAdminBase):
+    pass
