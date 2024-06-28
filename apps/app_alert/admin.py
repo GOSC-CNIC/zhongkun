a56b6104aba105c2642929e886a10f1993d40b3f
wenhao <whpang@cnic.cn>
2024-06-28 16:14:30 +0800

    告警模块，添加告警工单相关的模型到admin后台

--- apps/app_alert/admin.py
+++ apps/app_alert/admin.py
@@ -7,6 +7,13 @@ from django.utils import timezone as dj_timezone
 
 from utils.model import BaseModelAdmin
 from apps.app_alert.models import AlertMonitorJobServer, ResolvedAlertModel, AlertModel
+from apps.app_alert.models import TicketResolutionCategory
+from apps.app_alert.models import TicketResolution
+from apps.app_alert.models import AlertTicket
+from apps.app_alert.models import ServiceMetric
+from apps.app_alert.models import ServiceLog
+from apps.app_alert.models import AlertService
+from apps.app_alert.models import ServiceAdminUser
 
 
 def ts_to_time_dispaly(ts: Union[int, float]) -> str:
@@ -41,7 +48,7 @@ class AlertAdminBase(BaseModelAdmin):
     list_display = ['id', 'name', 'type', 'status', 'severity',
                     'start', 'show_start', 'end', 'show_end', 'show_recovery', 'count',
                     'instance', 'port', 'cluster', 'fingerprint',
-                    'show_creation', 'show_first_notification', 'show_last_notification', 'order_id',
+                    'show_creation', 'show_first_notification', 'show_last_notification', 'ticket_id',
                     'summary', 'description'
                     ]
     list_display_links = ('id', 'name')
@@ -80,3 +87,135 @@ class AlertModelAdmin(AlertAdminBase):
 @admin.register(ResolvedAlertModel)
 class ResolvedAlertModelAdmin(AlertAdminBase):
     pass
+
+
+@admin.register(TicketResolutionCategory)
+class TicketResolutionCategoryAdmin(BaseModelAdmin):
+    list_display = [
+        'id',
+        'name',
+        'creation',
+        'modification',
+    ]
+
+    list_display_links = [
+        'id',
+        'name',
+
+    ]
+
+
+@admin.register(TicketResolution)
+class TicketResolutionAdmin(BaseModelAdmin):
+    list_display = [
+        'id',
+        'category',
+        'resolution',
+        'creation',
+        'modification',
+    ]
+
+    list_display_links = [
+        'id',
+        'category',
+        'resolution',
+
+    ]
+
+
+@admin.register(AlertTicket)
+class TicketResolutionAdmin(BaseModelAdmin):
+    list_display = [
+        'id',
+        'title',
+        'description',
+        'severity',
+        'status',
+        'creation',
+        'modification',
+        'submitter',
+        'assigned_to',
+        'resolution',
+    ]
+
+    list_display_links = [
+        'id',
+        'title',
+    ]
+
+
+@admin.register(ServiceMetric)
+class ServiceMetricAdmin(BaseModelAdmin):
+    list_display = [
+        'id',
+        'name',
+        'name_en',
+        'job_tag',
+        'creation',
+        'sort_weight',
+    ]
+    list_display_links = (
+        'id',
+        'name',
+    )
+
+    filter_horizontal = (
+        "users",
+    )
+    raw_id_fields = ('service',)
+
+
+@admin.register(ServiceLog)
+class ServiceLogAdmin(BaseModelAdmin):
+    list_display = [
+        'id',
+        'name',
+        'name_en',
+        'job_tag',
+        'creation',
+        'sort_weight',
+    ]
+    list_display_links = (
+        'id',
+        'name',
+    )
+
+    filter_horizontal = (
+        "users",
+    )
+    raw_id_fields = ('service',)
+
+
+@admin.register(AlertService)
+class AlertServiceAdmin(BaseModelAdmin):
+    list_display = [
+        'id',
+        'name',
+        'name_en',
+        'creation_time',
+        'sort_weight',
+        'remark',
+    ]
+    list_display_links = (
+        'id',
+        'name',
+    )
+
+    filter_horizontal = (
+        "users",
+    )
+
+
+@admin.register(ServiceAdminUser)
+class ServiceAdminUserAdmin(BaseModelAdmin):
+    list_display = [
+        'id',
+        'userprofile',
+        'service',
+        'role',
+        'join_time',
+    ]
+    list_display_links = (
+        'id',
+        'userprofile',
+    )
