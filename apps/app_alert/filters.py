81d1965e1401ddbb5c5dd87e18aef83bad815935
wenhao <whpang@cnic.cn>
2024-07-11 12:29:40 +0800

    告警模块，查询告警列表接口移除has_ticket查询字段

--- apps/app_alert/filters.py
+++ apps/app_alert/filters.py
@@ -8,7 +8,6 @@ class AlterFilter(django_filters.FilterSet):
     id = django_filters.CharFilter(field_name="id", method="id_filter")
     status = django_filters.CharFilter(field_name="end", method="status_filter")
     alert_type = django_filters.CharFilter(field_name="type", lookup_expr='exact')
-    has_ticket = django_filters.CharFilter(field_name="ticket", method="has_ticket_filter")
 
     def status_filter(self, queryset, field_name, value):
         if value.lower() == AlertStatus.FIRING.value:
@@ -17,13 +16,6 @@ class AlterFilter(django_filters.FilterSet):
         elif value.lower() == AlertStatus.RESOLVED.value:
             queryset = queryset.filter(status=AlertStatus.RESOLVED.value)
             return queryset
-
-    def has_ticket_filter(self, queryset, field_name, value):
-        if value == 'true':
-            queryset = queryset.filter(ticket__isnull=False)
-        elif value == 'false':
-            queryset = queryset.filter(ticket__isnull=True)
-        return queryset
 
     def id_filter(self, queryset, field_name, value):
         return queryset.filter(id__istartswith=value)
@@ -48,8 +40,3 @@ class AlertFilterBackend(DjangoFilterBackend):
         Return the `FilterSet` class used to filter the queryset.
         """
         return AlterFilter
-
-
-
-
-
