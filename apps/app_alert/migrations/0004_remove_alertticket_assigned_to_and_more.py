630001c94f3131f32bfcccfe8902181dbd952b07
yushunwang <869588058@qq.com>
2024-07-12 14:39:22 +0800

    告警，优化0004和0005迁移文件

--- apps/app_alert/migrations/0004_remove_alertticket_assigned_to_and_more.py
+++ apps/app_alert/migrations/0004_remove_alertticket_assigned_to_and_more.py
@@ -22,29 +22,29 @@ class Migration(migrations.Migration):
             name='status',
             field=models.CharField(choices=[('accepted', '已受理'), ('changed', '已转移'), ('closed', '已完成')], default='accepted', max_length=16, verbose_name='状态'),
         ),
-        migrations.CreateModel(
-            name='TicketHandler',
-            fields=[
-                ('id', models.CharField(blank=True, editable=False, max_length=36, primary_key=True, serialize=False, verbose_name='ID')),
-                ('creation', models.DateTimeField(auto_now_add=True, help_text='提交时间', verbose_name='提交时间')),
-                ('modification', models.DateTimeField(auto_now=True, help_text='修改时间', verbose_name='修改时间')),
-                ('ticket', models.ForeignKey(db_constraint=False, on_delete=django.db.models.deletion.CASCADE, related_name='handler_set', related_query_name='handler', to='app_alert.alertticket', verbose_name='所属的工单')),
-                ('user', models.ForeignKey(db_constraint=False, null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='%(app_label)s_%(class)s_related', related_query_name='%(app_label)s_%(class)ss', to=settings.AUTH_USER_MODEL, verbose_name='处理人')),
-            ],
-            options={
-                'verbose_name': '告警工单处理人',
-                'verbose_name_plural': '告警工单处理人',
-                'db_table': 'alert_ticket_handler',
-                'ordering': ['-creation'],
-            },
-        ),
-        migrations.AddField(
-            model_name='alertticket',
-            name='handlers',
-            field=models.ManyToManyField(related_name='+', through='app_alert.TicketHandler', to=settings.AUTH_USER_MODEL, verbose_name='管理员'),
-        ),
-        migrations.AddConstraint(
-            model_name='tickethandler',
-            constraint=models.UniqueConstraint(fields=('ticket', 'user'), name='unique_together_ticket_user'),
-        ),
+        # migrations.CreateModel(
+        #     name='TicketHandler',
+        #     fields=[
+        #         ('id', models.CharField(blank=True, editable=False, max_length=36, primary_key=True, serialize=False, verbose_name='ID')),
+        #         ('creation', models.DateTimeField(auto_now_add=True, help_text='提交时间', verbose_name='提交时间')),
+        #         ('modification', models.DateTimeField(auto_now=True, help_text='修改时间', verbose_name='修改时间')),
+        #         ('ticket', models.ForeignKey(db_constraint=False, on_delete=django.db.models.deletion.CASCADE, related_name='handler_set', related_query_name='handler', to='app_alert.alertticket', verbose_name='所属的工单')),
+        #         ('user', models.ForeignKey(db_constraint=False, null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='%(app_label)s_%(class)s_related', related_query_name='%(app_label)s_%(class)ss', to=settings.AUTH_USER_MODEL, verbose_name='处理人')),
+        #     ],
+        #     options={
+        #         'verbose_name': '告警工单处理人',
+        #         'verbose_name_plural': '告警工单处理人',
+        #         'db_table': 'alert_ticket_handler',
+        #         'ordering': ['-creation'],
+        #     },
+        # ),
+        # migrations.AddField(
+        #     model_name='alertticket',
+        #     name='handlers',
+        #     field=models.ManyToManyField(related_name='+', through='app_alert.TicketHandler', to=settings.AUTH_USER_MODEL, verbose_name='管理员'),
+        # ),
+        # migrations.AddConstraint(
+        #     model_name='tickethandler',
+        #     constraint=models.UniqueConstraint(fields=('ticket', 'user'), name='unique_together_ticket_user'),
+        # ),
     ]
