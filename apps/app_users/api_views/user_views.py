ef10b16d1b1fd43b340bfae09173c0df7cc04467
yushunwang <869588058@qq.com>
2025-01-13 16:38:23 +0800

    用户，新增 修改用户所属机构接口和测试用例

--- apps/app_users/api_views/user_views.py
+++ apps/app_users/api_views/user_views.py
@@ -6,9 +6,12 @@ from rest_framework.serializers import Serializer
 from drf_yasg.utils import swagger_auto_schema, no_body
 from drf_yasg import openapi
 
+from core import errors
+from utils.paginators import NoPaginatorInspector
 from apps.api.viewsets import CustomGenericViewSet
 from apps.api.paginations import NewPageNumberPagination100
 from apps.app_users import serializers
+from apps.app_service.models import DataCenter as Organization
 
 
 class UserViewSet(CustomGenericViewSet):
@@ -20,6 +23,7 @@ class UserViewSet(CustomGenericViewSet):
 
     @swagger_auto_schema(
         operation_summary=gettext_lazy('获取用户个人信息'),
+        paginator_inspectors=[NoPaginatorInspector],
         responses={
             200: ''
         }
@@ -35,13 +39,66 @@ class UserViewSet(CustomGenericViewSet):
               "username": "admin",
               "fullname": "张三",
               "is_fed_admin": true, # true(联邦管理员)
-              "": {
+              "organization": {
                 "id": "xx",
                 "name": "xx",
                 "name_en": "xx"
               }
             }
         """
+        serializer = serializers.UserSerializer(instance=request.user)
+        return Response(data=serializer.data)
+
+    @swagger_auto_schema(
+        operation_summary=gettext_lazy('更改用户的机构'),
+        request_body=no_body,
+        manual_parameters=[
+            openapi.Parameter(
+                name='organization_id',
+                in_=openapi.IN_QUERY,
+                type=openapi.TYPE_STRING,
+                required=False,
+                description=gettext_lazy('机构ID，空值表示清除机构')
+            )
+        ],
+        responses={
+            200: ''
+        }
+    )
+    @action(methods=['post'], detail=False, url_path='change-org', url_name='change-org')
+    def change_org(self, request, *args, **kwargs):
+        """
+        更改用户的机构
+
+            Http Code: 状态码200，返回数据：
+            {
+              "id": "c172f4b8-984d-11eb-b920-90b11c06b9df",
+              "username": "admin",
+              "fullname": "张三",
+              "is_fed_admin": true, # true(联邦管理员)
+              "organization": {
+                "id": "xx",
+                "name": "xx",
+                "name_en": "xx"
+              }
+            }
+        """
+        organization_id = request.query_params.get('organization_id')
+        if organization_id is None:
+            return self.exception_response(
+                exc=errors.BadRequest(message=_('必须提交机构ID参数')))
+
+        if organization_id:
+            org = Organization.objects.filter(id=organization_id).first()
+            if org is None:
+                return self.exception_response(
+                    exc=errors.InvalidArgument(message=_('机构不存在')))
+        else:
+            org = None
+
+        user = request.user
+        user.organization = org
+        user.save(update_fields=['organization'])
         serializer = serializers.UserSerializer(instance=request.user)
         return Response(data=serializer.data)
 
--- apps/app_users/tests/test_user.py
+++ apps/app_users/tests/test_user.py
@@ -1,3 +1,5 @@
+from urllib import parse
+
 from django.urls import reverse
 
 from utils.test import get_or_create_service, MyAPITestCase, get_or_create_user, get_or_create_organization
@@ -38,3 +40,32 @@ class UserTests(MyAPITestCase):
         self.assertIs(response.data['is_fed_admin'], False)
         self.assertKeysIn(['id', 'name', 'name_en'], response.data['organization'])
         self.assertEqual(response.data['organization']['name'], '机构test')
+
+    def test_change_org(self):
+        org = get_or_create_organization(name='机构test')
+        base_url = reverse('users-api:user-change-org')
+        response = self.client.post(base_url)
+        self.assertErrorResponse(status_code=401, code='NotAuthenticated', response=response)
+
+        self.client.force_login(self.user)
+        response = self.client.post(base_url)
+        self.assertErrorResponse(status_code=400, code='BadRequest', response=response)
+
+        query = parse.urlencode(query={'organization_id': 'notfound'})
+        response = self.client.post(f'{base_url}?{query}')
+        self.assertErrorResponse(status_code=400, code='InvalidArgument', response=response)
+
+        self.user.refresh_from_db()
+        self.assertIsNone(self.user.organization)
+        query = parse.urlencode(query={'organization_id': org.id})
+        response = self.client.post(f'{base_url}?{query}')
+        self.assertEqual(response.status_code, 200)
+        self.assertKeysIn(["id", "username", "fullname", "organization"], response.data)
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.organization_id, org.id)
+
+        query = parse.urlencode(query={'organization_id': ''})
+        response = self.client.post(f'{base_url}?{query}')
+        self.assertEqual(response.status_code, 200)
+        self.user.refresh_from_db()
+        self.assertIsNone(self.user.organization)
