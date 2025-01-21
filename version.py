f415509f02635f010f8ec32caeb6d4452bd478c4
yushunwang <869588058@qq.com>
2025-01-21 11:17:03 +0800

    添加v3.2.0版本信息

--- version.py
+++ version.py
@@ -3,7 +3,7 @@ import subprocess
 import datetime
 
 
-VERSION = (3, 1, 2, 'final', 0)     # 'alpha', 'beta', 'rc', 'final'
+VERSION = (3, 2, 0, 'final', 0)     # 'alpha', 'beta', 'rc', 'final'
 
 
 def get_version(version=None):
